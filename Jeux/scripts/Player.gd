78ed1db63baae8235129efa4f8347bb935ae12c4
Sam <samolichampagne@icloud.com>
2023-11-19 19:07:25 -0500

    blabla

---
+++ Jeux/scripts/Player.gd
@@ -0,0 +1,13 @@
+extends CharacterBody2D
+
+var speed = 150
+
+func get_input():
+	var input_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	velocity = input_direction * speed
+
+func _physics_process(delta):
+	get_input()
+	move_and_slide() 
+	
+	
