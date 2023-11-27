8ae24a76a76f339cca5098cbe5ca6543cfe73d5c
chapsjust <2238424@carrefour.cegepvicto.ca>
2023-11-27 17:22:45 -0500

    big player update

--- Jeux/scripts/Player.gd
+++ Jeux/scripts/Player.gd
@@ -5,66 +5,70 @@ var speed = 80
 var last_direction = "idle_front"
 var directions = "front"
 var test = true
-func get_input():
-	var input_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var anim = $AnimatedSprite2D
-	var attack_anim = $Attack_anim
-	velocity = input_direction * speed
-	
-	#animation
-	if Input.is_action_pressed("ui_down"):
-		anim.play("walk_front")
-		last_direction = "idle_front"
-		directions = "front"
-	elif Input.is_action_pressed("ui_up"):
-		anim.play("walk_back")
-		last_direction = "idle_back"
-		directions = "back"
-	elif Input.is_action_pressed("ui_right"):
-		anim.flip_h = false
-		anim.play("walk_side")
-		last_direction = "idle_side"
-		directions = "right"
-	elif Input.is_action_pressed("ui_left"):
-		anim.flip_h = true
-		anim.play("walk_side")
-		last_direction = "idle_side"
-		directions = "left"
-	elif velocity == Vector2.ZERO:
-		anim.play(last_direction)
-	else:
-		anim.play(last_direction)
-		
-	if Input.is_action_just_pressed("souris"):
-		attack_anim.global_rotation = get_global_rotation()
-		if directions == "right":
-			attack_anim.position = Vector2(16, 4)
-			attack_anim.rotation_degrees = 6.5
-		elif directions == "front":
-			attack_anim.position = Vector2(-2, 18)
-			attack_anim.rotation_degrees = 96.9
-		elif directions == "back":
-			attack_anim.position = Vector2(-2, -18)
-			attack_anim.rotation_degrees = 276.9
-		elif directions == "left":
-			attack_anim.position = Vector2(-16, 4)
-			attack_anim.rotation_degrees = 186.5
-		attack_anim.visible = true
-		attack_anim.play("attack")
-	else:
-		attack_anim.visible = false
-			
+var can_attack = true
+
+func _ready():
+	$Attack_Sword/Attack_anim.visible = false
 
 func _physics_process(delta):
 	get_input()
 	move_and_slide()
 	update_health()
+
+func get_input():
+	var anim_player = $AnimatedSprite2D
+	var attack = $Attack_Sword
+
+	#Les mouvement en 8-way
+	var input_direction = Input.get_vector("A", "D", "W", "S")
+	velocity = input_direction * speed
 	
-	if vie <= 0 :
-		print("le personnage est mort")
-		self.queue_free()
-		get_tree().change_scene_to_file("res://Scene/game_over.tscn")
-# il faut créer ennemie
+	#Tout se qui concerne le player
+	if Input.is_action_pressed("S"):
+		anim_player.play("walk_front")
+		last_direction = "idle_front"
+		directions = "front"
+	elif Input.is_action_pressed("W"):
+		anim_player.play("walk_back")
+		last_direction = "idle_back"
+		directions = "back"
+	elif Input.is_action_pressed("D"):
+		anim_player.flip_h = false
+		anim_player.play("walk_side")
+		last_direction = "idle_side"
+		directions = "right"
+	elif Input.is_action_pressed("A"):
+		anim_player.flip_h = true
+		anim_player.play("walk_side")
+		last_direction = "idle_side"
+		directions = "left"
+	elif velocity == Vector2.ZERO:
+		anim_player.play(last_direction)
+	else:
+		anim_player.play(last_direction)
+		
+	#Les input de l'attaque et de l'area 2d
+	if Input.is_action_just_pressed("souris") and can_attack:
+		attack.global_rotation = get_global_rotation()
+		if directions == "right":
+			attack.position = Vector2(16, 4)
+			attack.rotation_degrees = 6.5
+		elif directions == "front":
+			attack.position = Vector2(-2, 18)
+			attack.rotation_degrees = 96.9
+		elif directions == "back":
+			attack.position = Vector2(-2, -18)
+			attack.rotation_degrees = 276.9
+		elif directions == "left":
+			attack.position = Vector2(-16, 4)
+			attack.rotation_degrees = 186.5
+		can_attack = false
+		$Attack_Sword/Attack_anim.visible = true
+		$Attack_Sword/Attack_anim.play("attack")
+		get_node("Attack_Sword/CollisionShape2D").disabled = false
+		$Slash.play()
+
+#Rafraichie la vie et vérifie si mort
 func update_health():
 	var lifebar = $lifebar
 	lifebar.value = vie
@@ -75,6 +79,11 @@ func update_health():
 	else :
 		lifebar.visible = true
 
+	#Vérifie si mort si oui mort affiche le game over
+	if vie <= 0 :
+		print("le personnage est mort")
+		self.queue_free()
+		get_tree().change_scene_to_file("res://Scene/game_over.tscn")
 
 func _on_area_2d_body_entered(body):
 	if body is Enemie:
@@ -82,3 +91,8 @@ func _on_area_2d_body_entered(body):
 		print(vie)
 		vie = vie - 10
 		print(vie)
+
+func _on_attack_anim_animation_finished():
+	$Attack_Sword/Attack_anim.visible = false
+	get_node("Attack_Sword/CollisionShape2D").disabled = true
+	can_attack = true
--- Jeux/scripts/game_over.gd
+++ Jeux/scripts/game_over.gd
@@ -1,7 +1,4 @@
 extends Control
-
-
-
 # pour relancer le jeu ps: a modifier ses pas optimal
 func _on_button_pressed():
 	get_tree().change_scene_to_file("res://Scene/Main.tscn")
