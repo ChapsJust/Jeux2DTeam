c5d17b3d909bb3e913f5d4ad6a6919ab5a2da245
Sam <samolichampagne@icloud.com>
2023-11-27 21:11:31 -0500

    patch level2

---
+++ Jeux/scripts/interfacemenu.gd
@@ -0,0 +1,10 @@
+extends Control
+
+
+
+func _on_lancer_pressed():
+	get_tree().change_scene_to_file("res://Scene/Main.tscn")
+
+
+func _on_tuto_pressed():
+	get_tree().change_scene_to_file("res://Scene/Main.tscn")
