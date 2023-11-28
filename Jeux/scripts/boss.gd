80613a70be62a0f1e57207ad0c25d3d729204b3f
chapsjust <2238424@carrefour.cegepvicto.ca>
2023-11-27 22:28:41 -0500

    audioboss

--- Jeux/scripts/boss.gd
+++ Jeux/scripts/boss.gd
@@ -11,6 +11,20 @@ func _process(delta):
 func take_damage(damage_amount):
 	vie_boss -= damage_amount
 	print(vie_boss)
+	bruitage()
 
 func bruitage():
-	pass
+	print(vie_boss)
+	if vie_boss <= 400 and vie_boss >= 380:
+		$cantkillme.play()
+		print("hey")
+	elif vie_boss <= 300 and vie_boss >= 290:
+		$imthegod.play()
+	elif vie_boss <= 150 and vie_boss >= 130:
+		$shalldie.play()
+	elif  vie_boss <= 0:
+		$comeback.play()
+
+
+func _on_comeback_finished():
+	queue_free()
--- Jeux/scripts/caverne.gd
+++ Jeux/scripts/caverne.gd
@@ -5,7 +5,6 @@ extends Node
 func _ready():
 	$boss/welcomehome.play()
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
